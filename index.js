f4405f63f3c2bd06ac23151ae31e4555ecf2a7c2
sbsohag3 <sbsohag3@gmail.com>
2022-07-02 19:50:55 +0600

    added

--- index.js
+++ index.js
@@ -2,10 +2,47 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r2lfm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+    const dataCollection = client.db("dailytask").collection("data");
+
+    app.get("/data", async (req, res) => {
+      const query = {};
+      const cursor = dataCollection.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
+
+    app.post("/createTodo", async (req, res) => {
+      const todo = req.body;
+      const result = await dataCollection.insertOne(todo);
+      res.send(result);
+    });
+
+  } finally {}
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
